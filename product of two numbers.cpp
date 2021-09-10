9063f918a2ea03bcbd99866b1c925fdfce7b71e7
kiran0306 <88666467+kiran0306@users.noreply.github.com>
2021-09-10 11:23:44 +0530

    create product of two numbers.cpp
    this screen shows the product of two numbers on the screen.

---
+++ product of two numbers.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+main()
+{
+	float n1,n2,product;
+	cout<<"enter a two numbers";
+	cin>>n1>>n2;
+	product=n1*n2;
+	cout<<"product :"<<product;
+	return 0;
+}
