43174a3aee30153e33381c7ae723c1f47f28f03e
kiran0306 <88666467+kiran0306@users.noreply.github.com>
2021-09-16 21:07:37 +0530

    create eligible or not for votting.cpp
    this program shows the persion is eligible or not for votting on the screen

---
+++ eligible or not for votting .cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int age;
+	if(age>=18)
+	{
+	 cout<<"persion is  eligible for vote \n";
+	}
+	else
+	{
+	 cout<<"persion is  eligible for vote \n";
+    }
+	return(0);
+}
